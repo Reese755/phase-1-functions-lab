09bbb4d99746b1a7df86eca995bed00573d84a2d
Reese Marshall <reese.amarshall@gmail.com>
2022-12-13 12:09:13 -0500

    commit tree a070fedc3c9194ce1892dcb43ecc57c72d3b607b

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42)
+}
+
+function distanceFromHqInBlocks(location) {
+    return distanceFromHqInBlocks(location) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {}
